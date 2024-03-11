80023e6afe222c95aeb45096877d333248cde927
Toyash <toyashpatil17@gmail.com>
2024-03-11 22:04:35 +0530

    refactor:landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,13 +3,13 @@ import Health from "../assets/Images/Health2.svg"
 import Nurse from "../assets/Images/nurse2.svg"
 import { useNavigate } from 'react-router-dom'
 const Landing = () => {
-    const navigate=useNavigate()
-    const handleClick=()=>{
+    const navigate = useNavigate()
+    const handleClick = () => {
         navigate('/signup')
     }
     return (
         <div className='h-[100vh] gradient_wall flex items-center justify-center overflow-hidden w-[100vw]'>
-            <div className=' w-[100%] space-y-10'>
+            <div className=' w-[100%] space-y-8'>
                 <div className=' flex items-center justify-center'>
                     <img src={Health}></img>
                 </div>
@@ -17,19 +17,24 @@ const Landing = () => {
                     <img src={Nurse} ></img>
                 </div>
                 <div className=' flex items-center justify-center'>
-                    <h1 className='text-3xl'>MEDXPERT</h1>
+                    <h1 className=' font-bold text-[40px]'>MEDXPERT</h1>
                 </div>
                 <div className=' flex items-center justify-center'>
-                    <h1>One-Stop Solution to All your Health Care need</h1>
+                    <div className='  justify-center'>
+                        <h1 className=' font-semibold text-center'>One-Stop <span className='text-[#734F96] font-bold underline'>Solution</span> </h1>
+                        <h1 className='font-bold'>To All your Health Care need</h1>
+                    </div>
+
+
                 </div>
                 <div className='   w-[100%] flex items-center justify-center'>
                     <div className='flex rounded-lg shadow-md drop-shadow-md  p-3  w-[80%] items-center gradient_button justify-center'>
-                        <button onClick={handleClick} className='text-xl'>SignUp</button>
+                        <button onClick={handleClick} className='text-xl font-bold text-white '>SignUp</button>
                     </div>
                 </div>
                 <div className=' w-[100%] flex items-center justify-center'>
-                    <div className='flex drop-shadow-md shadow-md rounded-lg  p-3  w-[80%] items-center gradient_button justify-center'>
-                        <button className='text-xl'>Login</button>
+                    <div className='flex drop-shadow-md border-4 border-[#8771EB] shadow-md rounded-lg  p-3  w-[80%] items-center justify-center'>
+                        <button className='text-xl font-bold text-black'>Login</button>
                     </div>
                 </div>
 
