c07af9e936b9fe248ca82e791d64bb2a0e35b72e
Toyash <toyashpatil17@gmail.com>
2024-03-12 02:25:38 +0530

    init:Backend deploy

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
-const app =express();
+const app = express();
 
 
 
 const PORT = 5000;
 
-app.get('/',(req,res)=>{
-    res.json({Sucess:"Server Started Successfully"})
+app.get('/', (req, res) => {
+    res.json({ Sucess: "Server Started Successfully" })
 })
 
-app.listen(PORT,()=>{
+app.get('/test', (req, res) => {
+    res.json({ Sucess: "Server Started Successfully on Test" })
+})
+
+
+app.listen(PORT, () => {
     console.log("Server Started on : " + PORT)
 })
