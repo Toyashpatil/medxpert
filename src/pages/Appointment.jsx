ac54209bcdde7f43cd8fd642105d00ef9d9a55ac
smitpatil0411 <smit.p@somaiya.edu>
2024-03-13 12:59:33 +0530

    feat:appoint

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -10,7 +10,7 @@ const Appointment = () => {
     
     return (
 
-        <div className='h-fit gradient_wall p-4 flex items-start justify-center  w-[100vw]'>
+        <div className='h-[100vh] gradient_wall p-4 flex items-start justify-center  w-[100vw]'>
             <div className=' w-[100%] space-y-2'>
                 {/* <div></div> */}
                 <div className=' flex items-centre justify-start'>
@@ -31,6 +31,11 @@ const Appointment = () => {
                         <div className='flex items-center justify-center'>{state.rating} </div>
                     </div>
 
+                </div>
+                <div className='   w-[100%] flex items-center justify-center '>
+                    <div className='mt-[15px] flex rounded-lg shadow-md drop-shadow-md  p-3  w-[80%] items-center gradient_button justify-center'>
+                        <button  className='text-xl font-bold text-white '>Book an Appointment</button>
+                    </div>
                 </div>
                 </div>
                 </div>
